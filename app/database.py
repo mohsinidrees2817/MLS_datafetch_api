3fc5ac489c6728fefc3f341aa0fdaf70b944d7fc
Mohsin Idrees <mohsinidrees2817@gmail.com>
2024-07-10 01:10:13 +0500

    docker added

--- app/database.py
+++ app/database.py
@@ -4,14 +4,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = (
-    f"DRIVER={{{os.getenv('DB_DRIVER')}}};"
-    f"SERVER={os.getenv('DB_SERVER')};"
-    f"DATABASE={os.getenv('DB_NAME')};"
-    f"UID={os.getenv('DB_USER')};"
-    f"PWD={os.getenv('DB_PASSWORD')};"
-    "Encrypt=yes;"
-)
-
+DATABASE_URL = os.getenv("DATABASE_URL")
 def get_db_connection():
     return pyodbc.connect(DATABASE_URL)
